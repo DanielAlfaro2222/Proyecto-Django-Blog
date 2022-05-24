faceb8cf7d37d71d2f5c8a0db53524169f73bd5f
Daniel Alfaro <kdalfaro@misena.edu.co>
2022-05-23 20:09:10 -0500

    Style: Se arreglan estilos de los articulos

--- Django_Blog/static/js/index.js
+++ Django_Blog/static/js/index.js
@@ -1,6 +1,17 @@
 'use strict';
 
 function editCommentary(button, article) {
-    article.childNodes[3].classList.toggle('container-form-edit-commentary--show');
+    article.childNodes[7].classList.toggle('container-form-edit-commentary--show');
+    article.childNodes[3].classList.toggle('container-commentary__paragraph--hidden');
+    article.childNodes[5].classList.toggle('container-commentary__paragraph--hidden');
+    article.childNodes[9].classList.toggle('container-actions-commentary--hidden');
+    button.classList.toggle('container-actions-commentary__link--hidden');
+}
+
+function cancelEditCommentary(button, article) {
+    article.childNodes[7].classList.toggle('container-form-edit-commentary--show');
+    article.childNodes[3].classList.toggle('container-commentary__paragraph--hidden');
+    article.childNodes[5].classList.toggle('container-commentary__paragraph--hidden');
+    article.childNodes[9].classList.toggle('container-actions-commentary--hidden');
     button.classList.toggle('container-actions-commentary__link--hidden');
 }
